c78434c0115223e6c5c20a5b8a1089ea82ea786f
Markov500 <markonolitse@gmail.com>
2023-05-04 21:01:54 +0000

    Implementation de l'UI de la page d'ajout de demande

--- lib/screens/ajout_demande.dart
+++ lib/screens/ajout_demande.dart
@@ -18,6 +18,8 @@ class _AjoutDemandeState extends State<AjoutDemande> {
   TextEditingController motifController = TextEditingController();
   TextEditingController contenuController = TextEditingController();
   DateTime? creneau;
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +27,7 @@ class _AjoutDemandeState extends State<AjoutDemande> {
         title: Text("Faire une demande"),
       ),
       body: Form(
+        key: formKey,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.start,
@@ -66,6 +69,9 @@ class _AjoutDemandeState extends State<AjoutDemande> {
                   border: OutlineInputBorder(),
                   hintText: 'Taper le motif',
                 ),
+                validator: (value) {
+                  return value == "" ? "Ce champ est obligatoire" : null;
+                },
               ),
             ),
 
@@ -80,6 +86,9 @@ class _AjoutDemandeState extends State<AjoutDemande> {
                   border: OutlineInputBorder(),
                   hintText: 'Taper le contenu',
                 ),
+                validator: (value) {
+                  return value == "" ? "Ce champ est obligatoire" : null;
+                },
               ),
             ),
 
@@ -93,11 +102,14 @@ class _AjoutDemandeState extends State<AjoutDemande> {
                 padding: EdgeInsets.only(left: 15, top: 20, bottom: 20),
                 margin: EdgeInsets.all(10),
                 alignment: Alignment.centerLeft,
-                child: Text("Choisir un crenau"),
                 decoration: BoxDecoration(
-                    border: Border.all(
-                  color: Colors.black38,
-                )),
+                  border: Border.all(
+                    color: Colors.black38,
+                  ),
+                ),
+                child: Text(creneau == null
+                    ? "Choisir un crenau"
+                    : formatDate(creneau!)),
               ),
               onTap: () {
                 showDatePicker(
@@ -116,12 +128,13 @@ class _AjoutDemandeState extends State<AjoutDemande> {
             Center(
               child: ElevatedButton(
                 onPressed: () {
+                  /*
                   Demande(
                       id: DateTime.now().toString(),
                       motif: motifController.text,
                       nomUtilisateur: ami,
                       description: contenuController.text,
-                      dateEnvoi: creneau!);
+                      dateEnvoi: creneau!);*/
                 },
                 child: Text("Valider"),
               ),
@@ -131,4 +144,58 @@ class _AjoutDemandeState extends State<AjoutDemande> {
       ),
     );
   }
+}
+
+String formatDate(DateTime date) {
+  //Format  the day
+  String res =
+      date.day < 2 ? "0" + date.day.toString() : date.day.toString() + " ";
+
+  //Format the month
+  switch (date.month) {
+    case 1:
+      res += "Jan";
+      break;
+    case 2:
+      res += "fev";
+      break;
+    case 3:
+      res += "Mar";
+      break;
+    case 4:
+      res += "Avr";
+      break;
+    case 5:
+      res += "Mai";
+      break;
+    case 6:
+      res += "Juin";
+      break;
+    case 7:
+      res += "Juil";
+      break;
+    case 8:
+      res += "Aou";
+      break;
+    case 9:
+      res += "Sep";
+      break;
+    case 10:
+      res += "Oct";
+      break;
+    case 11:
+      res += "Nov";
+      break;
+    case 12:
+      res += "Dec";
+      break;
+    default:
+      res += " ";
+      break;
+  }
+
+  //Format the year
+  res += " " + date.year.toString();
+
+  return res;
 }
